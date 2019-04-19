c27bb3f29c9ac7230c579f477b482f3af5af117a
Matthew Spellings <mspells@umich.edu>
2019-04-19 14:12:12 -0400

    Move m-major iteration guts into an encapsulated class

---
+++ src/internal.hpp
@@ -0,0 +1,66 @@
+
+#ifndef __FSPH_INTERNAL__
+#define __FSPH_INTERNAL__
+
+#ifndef __NVCC__
+#define __device__
+#define __host__
+#endif
+
+namespace fsph{
+    namespace internal{
+
+        template<typename Real, typename Complex>
+        class SPHSeriesEvaluator
+        {
+        public:
+            __device__ SPHSeriesEvaluator(const unsigned int &m)
+            {
+                reset(m);
+            }
+
+            __device__ void reset(const unsigned int &m)
+            {
+                m_m = 0;
+                m_jacobi_before_last = 0;
+
+                m_last_jacobi = 1.0;
+                for(unsigned int i(1); i <= m; i++)
+                    m_last_jacobi *= 1 + 0.5/i;
+                m_last_jacobi = sqrt(m_last_jacobi);
+                m_last_jacobi /= sqrt(2.0);
+            }
+
+            __device__ Complex get(const Real &sphi, const Real &cphi, const Real &theta, const unsigned int &l)
+            {
+                const Real k(l - m_m);
+                const Real prefactor_1(
+                    2*sqrt((1.0 + (m_m - 0.5)/k)*(1.0 - (m_m - 0.5)/(k + 2.0*m_m))));
+                const Real prefactor_2(
+                    sqrt((1.0 + 4.0/(2.0*k + 2.0*m_m - 3.0))*(1.0 - 1.0/k)*(1.0 - 1.0/(k + 2.0*m_m))));
+                const Real jacobi(
+                    prefactor_1*cphi*m_last_jacobi - prefactor_2*m_jacobi_before_last);
+
+                Complex Ylm(pow(sphi, m_m)/sqrt(2*M_PI)*jacobi);
+                Ylm *= exp(Complex(0, m_m*theta));
+
+                m_jacobi_before_last = m_last_jacobi;
+                m_last_jacobi = jacobi;
+
+                return Ylm;
+            }
+
+            __device__ Complex get_negative_m(const Complex &Ylm, const Real &theta)
+            {
+                return exp(Complex(0, -2.0*m_m*theta))*Ylm;
+            }
+
+        private:
+            unsigned int m_m;
+            Real m_last_jacobi;
+            Real m_jacobi_before_last;
+        };
+    }
+}
+
+#endif
--- src/tensorflow_op_gpu.cu
+++ src/tensorflow_op_gpu.cu
@@ -1,5 +1,6 @@
 
 #include "tensorflow_op_gpu.cu.hpp"
+#include "internal.hpp"
 #include "../cuda_complex/cuda_complex.hpp"
 
 namespace fsph{
@@ -19,13 +20,7 @@ namespace fsph{
 
         unsigned int m(threadIdx.x);
         unsigned int l(m);
-        Real jacobi_before_last(0);
-
-        Real last_jacobi(1);
-        for(unsigned int i(1); i <= m; i++)
-            last_jacobi *= 1 + 0.5/i;
-        last_jacobi = sqrt(last_jacobi);
-        last_jacobi /= sqrt(2.0);
+        fsph::internal::SPHSeriesEvaluator<Real, Complex> eval(m);
 
         for(unsigned int i(0); i < lmax + 1; i++)
         {
@@ -33,27 +28,10 @@ namespace fsph{
             {
                 m = lmax - threadIdx.x;
                 l = m;
-                jacobi_before_last = 0;
-                last_jacobi = 1;
-                for(unsigned int j(1); j <= m; j++)
-                    last_jacobi *= 1 + 0.5/j;
-                last_jacobi = sqrt(last_jacobi);
-                last_jacobi /= sqrt(2.0);
+                eval.reset(m);
             }
 
-            const Real k(l - m);
-            const Real prefactor_1(
-                2*sqrt((1.0 + (m - 0.5)/k)*(1.0 - (m - 0.5)/(k + 2.0*m))));
-            const Real prefactor_2(
-                sqrt((1.0 + 4.0/(2.0*k + 2.0*m - 3.0))*(1.0 - 1.0/k)*(1.0 - 1.0/(k + 2.0*m))));
-            const Real jacobi(
-                prefactor_1*cphi*last_jacobi - prefactor_2*jacobi_before_last);
-
-            Complex Ylm(pow(sphi, m)/sqrt(2*M_PI)*jacobi);
-            Ylm *= exp(Complex(0, m*theta));
-
-            jacobi_before_last = last_jacobi;
-            last_jacobi = jacobi;
+            const Complex Ylm(eval.get(sphi, cphi, theta, l));
 
             const unsigned int sph_per_point(
                 full_m? (lmax + 1)*(lmax + 2)/2 + lmax*(lmax + 1)/2:
@@ -64,7 +42,7 @@ namespace fsph{
             output[sph_index] = Ylm;
 
             if(full_m)
-                output[sph_index + l] = Ylm*exp(Complex(0, -2.0*m*theta));
+                output[sph_index + l] = eval.get_negative_m(Ylm, theta);
 
             l++;
         }
